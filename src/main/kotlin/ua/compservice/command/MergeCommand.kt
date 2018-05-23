c8de866985813255c6b2c952243b62d89a359b5b
dimkas71g@gmail.com <dimkas71g@gmail.com>
2018-05-23 23:34:41 +0300

    Adding MergeCommand to process merging many xlsx files to the output

--- src/main/kotlin/ua/compservice/command/MergeCommand.kt
+++ src/main/kotlin/ua/compservice/command/MergeCommand.kt
@@ -1,4 +1,29 @@
 package ua.compservice.command
 
-class MergeCommand {
+import com.beust.jcommander.Parameter
+import com.beust.jcommander.Parameters
+import ua.compservice.util.loggerFor
+
+
+@Parameters(commandNames = ["merge"], commandDescription = "Merge xlsx files into an one output file")
+data class MergeCommand(
+        @Parameter(description = "list of files to merge", required = true)
+        var files: List<String> = mutableListOf(),
+
+        @Parameter(names = ["-wt", "--with-team"], description = "if used then column TEAM will be inserted before the first timesheet's day(1)",
+                required = false)
+        var witTeam: Boolean = false,
+
+        @Parameter(names = ["-o", "--output"], description = "Optional output file, by default equals to output.xlsx", required = false)
+        var output: String = DEFAULT_OUT
+
+) {
+    fun merge() {
+        LOG.debug("{}", this)
+    }
+
+    companion object {
+        val LOG = loggerFor<MergeCommand>()
+        val DEFAULT_OUT = "output.xlsx"
+    }
 }
