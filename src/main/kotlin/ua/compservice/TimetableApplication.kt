85e910428c089ab28ed853a8935051665bf7d444
dimkas71g@gmail.com <dimkas71g@gmail.com>
2018-05-23 23:37:15 +0300

    Adding handler for merge command to the main app

--- src/main/kotlin/ua/compservice/TimetableApplication.kt
+++ src/main/kotlin/ua/compservice/TimetableApplication.kt
@@ -2,6 +2,7 @@ package ua.compservice
 
 import com.beust.jcommander.JCommander
 import ua.compservice.command.HelpCommand
+import ua.compservice.command.MergeCommand
 import ua.compservice.command.MergeSheetsCommand
 import ua.compservice.util.loggerFor
 
@@ -11,12 +12,14 @@ object TimetableApplication {
     @JvmStatic
     fun main(args: Array<String>) {
         val helpCommand = HelpCommand()
+        val mergeCommamnd = MergeCommand()
 
         val mergeSheetsCommand = MergeSheetsCommand()
 
         val commander = JCommander.newBuilder()
                 .addObject(this)
                 .addCommand(helpCommand)
+                .addCommand(mergeCommamnd)
                 .addCommand(mergeSheetsCommand)
                 .args(args)
                 .build()
@@ -25,6 +28,13 @@ object TimetableApplication {
 
         when (command) {
             "help" -> commander.usage()
+            "merge" -> {
+                mergeCommamnd.run {
+                    LOG.debug("{}", mergeCommamnd)
+                    mergeCommamnd.merge()
+                }
+
+            }
             "merge-sheets" -> {
                 mergeSheetsCommand.run {
                     LOG.debug("{}", mergeSheetsCommand)
