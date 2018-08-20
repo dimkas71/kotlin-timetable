9cf4270e8c42bc1dcdd4b41132c738cffb2ae0f8
dimkas71 <dimkas71g@gmail.com>
2018-08-20 15:31:48 +0300

    Fixing some minor changes

--- src/test/kotlin/ua/compservice/CellsTimeConerterTest.kt
+++ src/test/kotlin/ua/compservice/CellsTimeConerterTest.kt
@@ -6,29 +6,34 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvFileSource
 import ua.compservice.model.RuleItem
+import ua.compservice.model.findByStopNetcode
 import ua.compservice.model.load
 
 
 class `Cells timesheet converters` {
 
-    lateinit var rules: List<RuleItem>
-
-    @BeforeEach
-    fun setUp() {
-       rules = listOf<RuleItem>().load("src/test/resources/conversion_table.xlsx")
-    }
-
     @Test
     fun `first test`() {
-        Assertions.assertTrue(rules.size > 0, "Rules should be initialized...")
+        Assertions.assertTrue(RULES.size > 0, "Rules should be initialized...")
     }
 
     @ParameterizedTest
     @CsvFileSource(resources = ["/test.csv"], delimiter = ';')
     fun `test`(source: String, target: String) {
 
+        val classifier = REGEX.find(source)?.groups?.get("classifier")?.value ?: ""
+
+        val converted = RULES.find { it.stopNetCode.equals(classifier.trim()) }?.oneCCode
+
+        if (converted == null) println(classifier)
 
 
+
+    }
+
+    companion object {
+        val RULES = listOf<RuleItem>().load("src/test/resources/conversion_table.xlsx")
+        val REGEX = Regex("(?<classifier>[а-яА-ЯІіЇїЄєҐґ\\s\\/0-9]{1,})(\\((?<time>[0-9]{1}:[0-9]{2}|[-]{1})\\)){0,}")
     }
 
 }
--- src/test/kotlin/ua/compservice/RuleItemTest.kt
+++ src/test/kotlin/ua/compservice/RuleItemTest.kt
@@ -15,11 +15,14 @@ class RuleItemTest {
                 }.toList()
 
 
-        val r = Regex("(?<classifier>[а-яА-яІіЇїЄєҐґ\\s\\/0-9]{1,})(\\((?<time>[0-9]{1}:[0-9]{2}|[-]{1})\\)){0,}")
+        val r = Regex("(?<classifier>[а-яА-ЯІіЇїЄєҐґ\\s\\/0-9]{1,})(\\((?<time>[0-9]{1}:[0-9]{2}|[-]{1})\\)){0,}")
 
         list.map {
-            Pair(r.find(it)?.groups?.get("classifier")?.value, it)
+            Pair(r.find(it)?.groups?.get("time")?.value, it)
         }.forEach { println(it) }
 
+
     }
+
+
 }
